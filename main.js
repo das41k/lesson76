90dfd76a8dbcba0c8ece46213d394338610d7c52
das41k <148576114+das41k@users.noreply.github.com>
2023-11-03 22:16:56 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const myName = 'Armen'
+const age = '18'
+const city = 'Orenburg';
+const hobby = 'Watch Aroken';
+const pet = 'cat';
+console.log(`My name is ${myName}, I live in the ${city}, I am ${age} years old, My hobby is ${hobby}, I have a ${pet}`);
